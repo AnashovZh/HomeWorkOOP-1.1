485fd6135f14f58e6edf90a336c54f7a5f8ee9c9
user <anashov17@gmail.com>
2023-05-01 23:39:18 +0600

    OOP

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        /**   Triangle деген класс тузунуз.
+         Класстын 3 полеси болсун a, b, c.
+         Класстын "area" деген методу болсун.
+         "area" методу полелериндеги маалыматтарга(маалымат объектти тузгондон кийин берилет)
+         таянып консолго уч бурчтуктун аянтын чыгарсын.*/
+
+        Triangle triangle = new Triangle();
+        triangle.a = 15;
+        triangle.b = 20;
+        triangle.c = 10;
+        triangle.area();
+
+    }}
---
+++ src/Triangle.java
@@ -0,0 +1,10 @@
+public class Triangle {
+    double a;
+    double b;
+    double c;
+
+public  void area(){
+    double n=(a+b+c)/2;
+    System.out.println(Math.sqrt(n*(n-a)*(n-b)*(n-c)));
+
+}}
